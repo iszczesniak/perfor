5e7f5fae1a7d6adc794c02efb247be84bab205c9
Irek Szcześniak <irek.szczesniak@gmail.com>
2016-02-24 14:38:00 +0100

    added more properties to the graph

--- calc.cc
+++ calc.cc
@@ -23,7 +23,7 @@ calc::operator()()
   generate_pon (pon, m_a, gen);
   
   // Generate traffic.
-  generate_traffic (pon1, m_a, gen, m_uv);
+  generate_traffic (pon, m_a, gen, m_uv);
 
   // Calculate the mean ONU performance.
   double perfor = m_uv + m_seed;
--- graph.hpp
+++ graph.hpp
@@ -19,12 +19,24 @@ typedef std::set<int> SOO;
 enum class VERTEX_T {OLT, ONU, ICO, PRN, ARN};
 
 namespace boost {
+  // Vertex type.
   enum vertex_type_t {vertex_type};
+  // Downstream datarate required for vertex.
   enum vertex_drate_t {vertex_drate};
-  enum edge_onus_t {edge_onus};
+  // ONUs allocated downstream on the edge.
+  enum edge_donus_t {edge_donus};
+  // ONUs allocated upstream on the edge.
+  enum edge_uonus_t {edge_uonus};
+  // Available downstream datarate of an edge.
+  enum edge_drate_t {edge_drate};
+  // Available upstream datarate of an edge.
+  enum edge_urate_t {edge_urate};
   BOOST_INSTALL_PROPERTY(vertex, type);
   BOOST_INSTALL_PROPERTY(vertex, drate);
-  BOOST_INSTALL_PROPERTY(edge, onus);
+  BOOST_INSTALL_PROPERTY(edge, donus);
+  BOOST_INSTALL_PROPERTY(edge, uonus);
+  BOOST_INSTALL_PROPERTY(edge, drate);
+  BOOST_INSTALL_PROPERTY(edge, urate);
 }
 
 /**
@@ -34,9 +46,11 @@ typedef
 boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
                       boost::property<boost::vertex_name_t, std::string,
                       boost::property<boost::vertex_type_t, VERTEX_T,
-                      boost::property<boost::vertex_drate_t,
-                                      boost::optional<double> > > >,
-                      boost::property<boost::edge_onus_t, SOO> >
+                      boost::property<boost::vertex_drate_t, double> > >,
+                      boost::property<boost::edge_drate_t, double,
+                      boost::property<boost::edge_urate_t, double,
+                      boost::property<boost::edge_donus_t, SOO,
+                      boost::property<boost::edge_uonus_t, SOO> > > > >
 graph;
 
 typedef graph::edge_descriptor edge;
