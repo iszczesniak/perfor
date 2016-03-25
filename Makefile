ae725debaf80051b1c69bbf08ea9ea685a2c3159
Irek Szcześniak <irek.szczesniak@gmail.com>
2016-03-25 13:15:11 +0100

    net_fac

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 TARGETS = perfor
 LINK.o = $(LINK.cc)
 
-OBJS = graph.o perfor.o progress.o net_fac.o
+OBJS = graph.o perfor.o progress.o
 
 CXXFLAGS = -g
 #CXXFLAGS = -O3
--- net_fac.hpp
+++ net_fac.hpp
@@ -1,6 +1,8 @@
 #ifndef NET_FAC_HPP
 #define NET_FAC_HPP
 
+#include "args_net.hpp"
+
 #include <utility>
 
 // The network factory.
@@ -8,20 +10,23 @@ template<typename G>
 struct net_fac
 {
   typedef typename std::map<args_net<double>, G> a2n_type;
-  static a2n_type a2n;
+  static a2n_type m_a2n;
 
   static const G &
   get (const args_net<double> &args)
   {
-    auto i = a2n.find(args);
-    if (i == a2n.end())
+    auto i = m_a2n.find(args);
+    if (i == m_a2n.end())
       {
         // Generate the graph, because it was not generated before.
-        i = a2n.insert(std::make_pair(args, G())).first;
+        i = m_a2n.insert(std::make_pair(args, G())).first;
         generate_pon (i->second, args);
       }
     return i->second;
   }
 };
+
+template<typename G>
+typename net_fac<G>::a2n_type net_fac<G>::m_a2n;
 
 #endif /* NET_FAC_HPP */
