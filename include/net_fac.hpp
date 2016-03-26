ead4fc604dedf1d38f18646207af93fe38431727
Ireneusz Szcześniak <iszczesniak@fractal>
2016-03-26 21:00:48 +0100

    fixed a bug with the parallel execution

--- include/net_fac.hpp
+++ include/net_fac.hpp
@@ -3,6 +3,7 @@
 
 #include "args_net.hpp"
 
+#include <mutex>
 #include <utility>
 
 // The network factory.
@@ -11,10 +12,13 @@ struct net_fac
 {
   typedef typename std::map<args_net<double>, G> a2n_type;
   static a2n_type m_a2n;
+  static std::mutex m_a2n_mutex;
 
   static const G &
   get (const args_net<double> &args)
   {
+    m_a2n_mutex.lock ();
+
     auto i = m_a2n.find(args);
     if (i == m_a2n.end())
       {
@@ -22,11 +26,17 @@ struct net_fac
         i = m_a2n.insert(std::make_pair(args, G())).first;
         generate_pon (i->second, args);
       }
+
+    m_a2n_mutex.unlock ();
+
     return i->second;
   }
 };
 
 template<typename G>
 typename net_fac<G>::a2n_type net_fac<G>::m_a2n;
+
+template<typename G>
+typename std::mutex net_fac<G>::m_a2n_mutex;
 
 #endif /* NET_FAC_HPP */
