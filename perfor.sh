d43e3eaa7705185c4eb4047287c1b5811c3808d2
Irek Szcześniak <irek.szczesniak@gmail.com>
2016-04-12 15:14:45 +0200

    perfor

--- perfor.sh
+++ perfor.sh
@@ -1,7 +1,8 @@
+#PBS -q plgrid
 #PBS -l walltime=71:00:00
 #PBS -l nodes=1:ppn=12
 #PBS -l mem=32GB
 #PBS -o ~/perfor/results
 #PBS -j oe
 module load plgrid/libs/boost/1.59.0 > /dev/null 2>&1
-~/perfor/perfor
+~/perfor/perfor > ~/perfor/results
