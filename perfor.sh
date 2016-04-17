ffa9fd4ce5f69b2ebf521feeaf1a70eb63f3fe28
Irek Szcześniak <irek.szczesniak@gmail.com>
2016-04-17 16:24:39 +0200

    removing the PBS file

--- perfor.sh
+++
@@ -1,7 +0,0 @@
-#PBS -q plgrid
-#PBS -l walltime=71:00:00
-#PBS -l nodes=1:ppn=12
-#PBS -o ~/perfor/results
-#PBS -j oe
-module load plgrid/libs/boost/1.59.0 > /dev/null 2>&1
-~/perfor/perfor > ~/perfor/results
